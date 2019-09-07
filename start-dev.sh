ee60070b2f24f20ec77e5a376ff389b4c16cecf0
Ben Lowry <benlowry@outlook.com>
2019-09-07 17:40:03 -0600

    Finalizing

--- start-dev.sh
+++ start-dev.sh
@@ -2,7 +2,7 @@ NODE_ENV=development \
 PAGE_SIZE=3 \
 DASHBOARD_SERVER="http://localhost:8002" \
 PORT=8002 \
-node main.js
+node main.js --debug-brk=5858
 
 # Maxmind Geoip parameters
 # This module does not have any additional configuration settings.
