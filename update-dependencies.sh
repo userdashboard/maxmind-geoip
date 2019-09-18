f9bb16402c5d68f893f6acce530caa6f0cc8ebfe
benlowry <benlowry@outlook.com>
2019-09-18 10:27:58 -0600

    Removed dev dependencies

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -1,6 +1,4 @@
 npm update maxmind || exit 1
-npm update @userdashboard/dashboard || exit 1
-npm update puppeteer || exit 1
 npm shrinkwrap --dev || exit 1
 git add npm-shrinkwrap.json || exit 1
 git add package.json || exit 1
