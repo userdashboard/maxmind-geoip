8c8487894c641134f99d3a7146f980b1c8c4de7a
Ben Lowry <benlowry@pop-os.localdomain>
2020-05-27 15:52:31 -0600

    Clean up old files

--- start-dev.sh
+++
@@ -1,9 +0,0 @@
-NODE_ENV=development \
-PAGE_SIZE=3 \
-DASHBOARD_SERVER="http://localhost:8002" \
-PORT=8002 \
-node main.js --debug-brk=5858
-
-# Maxmind Geoip parameters
-# This module does not have any additional configuration settings.
-
--- update-dependencies.sh
+++
@@ -1,6 +0,0 @@
-npm update maxmind || exit 1
-npm shrinkwrap --dev || exit 1
-git add npm-shrinkwrap.json || exit 1
-git add package.json || exit 1
-git commit -m "Updated dependencies" || exit 1
-git push origin master || exit 1
