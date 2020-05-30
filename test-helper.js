cc364ef50351cac745dab343988a8867de42ba9c
Ben Lowry <benlowry@pop-os.localdomain>
2020-05-30 17:29:16 -0600

    Cleaning up

--- test-helper.js
+++ test-helper.js
@@ -4,6 +4,6 @@ global.applicationPath = global.applicationPath || __dirname
 const TestHelper = require('@userdashboard/dashboard/test-helper.js')
 module.exports = TestHelper
 
-before (async () => {
-    require('./index.js').setup()
+before(async () => {
+  require('./index.js').setup()
 })
