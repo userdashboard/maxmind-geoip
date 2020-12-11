c7f618b14eeebaaf24beac1ee499fe4ae467ce40
benlowry <benlowry@outlook.com>
2020-12-11 08:41:32 -0800

    Update MaxMind API

--- src/www/api/user/maxmind/country.js
+++ src/www/api/user/maxmind/country.js
@@ -8,7 +8,7 @@ module.exports = {
     if (!req.query || !req.query.ip) {
       throw new Error('invalid-ip')
     }
-    db = db || maxmind.openSync(path.join(__dirname, '../../../../GeoLite2-Country.mmdb'))
+    db = db || maxmind.open(path.join(__dirname, '../../../../GeoLite2-Country.mmdb'))
     if (process.env.NODE_ENV !== 'production' && req.query.ip === '127.0.0.1') {
       req.query.ip = '8.8.8.8'
     }
